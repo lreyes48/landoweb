2a878239c048cd0bb8adf20d90403e7f8bb11de2
Leandro <leandrothebest854@gmail.com>
2022-10-18 14:03:24 -0700

    new index, js, and cloud service stuff

---
+++ public/asset/js/main.js
@@ -0,0 +1,40 @@
+
+
+
+
+var input = document.getElementById("inputt");
+
+var input2 = document.getElementById("inputt").innerHTML;
+
+console.log(input2);
+
+window.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        console.log(input2);
+    }
+})
+
+function commander(cmd) {
+    switch (cmd.toLowerCase) {
+        case "help":
+    }
+}
+ 
+
+
+//if (input == "help") {
+    //let input2 = "helping";
+//}
+
+
+//input.addEventListener("keypress", (e) => {
+  //  if (e.key === 'Enter')
+    //console.log("s");
+    
+//})
+
+
+//inputt.oninput = showResult;
+//function showResult() {
+  //  test.innerText = this.value
+//}
