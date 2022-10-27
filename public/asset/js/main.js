189d0fa06c4aec815683a0b8bb223514f59bd810
that1mexicanguy <59989206+that1mexicanguy@users.noreply.github.com>
2022-10-27 14:09:33 -0700

    fixes

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,7 +1,8 @@
 
 const input = document.getElementById('inputt');
 const change = document.getElementById('test');
-const rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+const rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+const instagram = "https://www.instagram.com/leandroreyes__/";
 
 window.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
@@ -11,17 +12,26 @@ window.addEventListener("keyup", (event) => {
     }
 })
 function validate(event) {
-    if (input.value === "help") {
-        change.innerHTML = "secret, ... ";
+    if (input.value.toLowerCase() === "help"  ) {
+        change.innerHTML = "secret, links ... ";
     }
-    else if (input.value === "Chambon2022") {
+    else if (input.value.toLowerCase() === "Chambon2022") {
         window.open("Chambon2022", '_blank');
     }
-    else if (input.value === "amor <3") {
+    else if (input.value.toLowerCase() === "amor <3") {
         window.open("mcknz", '_blank');
     }
-    else if (input.value === "secret") {
-        window.open(rickroll, '_blank')
+    else if (input.value.toLowerCase() === "secret") {
+        window.open(rickroll, '_blank');
+    }
+    else if (input.value.toLowerCase() === "instagram") {
+        change.innerHTML = "Opening Instagram...";
+    }
+    else if (input.value.toLowerCase() === "links") {
+        change.innerHTML = "instagram";
+    }
+    else if (input.value.toLowerCase() === "clear") {
+        change.innerHTML = "";
     }
     else {
         console.log("nada")
