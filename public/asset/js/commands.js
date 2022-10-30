e2ce7a1bd17448b3759247ead2783f895c0771a8
that1mexicanguy <59989206+that1mexicanguy@users.noreply.github.com>
2022-10-30 00:42:14 -0700

    updates!!!

--- public/asset/js/commands.js
+++ public/asset/js/commands.js
@@ -1,3 +1,39 @@
-help = [
-    helping
-]
+const rootstuff = document.querySelector(':root');
+const input = document.getElementById('inputt');
+const change = document.getElementById('test');
+const gitty = document.getElementById("Github");
+const insta = document.getElementById("Instagram");
+const rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+const instagram = "https://www.instagram.com/leandroreyes__/";
+const github = "https://github.com/that1mexicanguy";
+var linksHelp = (['github',
+'instagram',
+'random,'
+])
+var heyyy = linksHelp.toString().length;
+var helpChar = ('secret, links, ...');
+
+const timevalue = parseInt(getComputedStyle(rootstuff).getPropertyValue('--time'));
+console.log(helpChar.length);
+function instachar() {
+    const characterLength = 20;
+    
+    rootstuff.style.setProperty('--steps', characterLength);
+}
+function helpchar2() {
+    const characterLength = 18;
+    rootstuff.style.setProperty('--steps', characterLength);
+}
+function gitchar() {
+    const characterLength = 17;
+    rootstuff.style.setProperty('--steps', characterLength);
+    
+}
+function instchar2() {
+    const characterLength = 9;
+    rootstuff.style.setProperty('--steps', characterLength);
+}
+function gittychar2() {
+    const characterLength = 6;
+    rootstuff.style.setProperty('--steps', characterLength);
+}
--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,8 +1,4 @@
-
-const input = document.getElementById('inputt');
-const change = document.getElementById('test');
-const rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-const instagram = "https://www.instagram.com/leandroreyes__/";
+//finetune the time it takes to type.
 
 window.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
@@ -13,32 +9,92 @@ window.addEventListener("keyup", (event) => {
 })
 function validate(event) {
     if (input.value.toLowerCase() === "help"  ) {
-        change.innerHTML = "secret, links ... ";
+        clear();
+        helpchar2();
+        document.activeElement.blur();
+        change.classList.add("textmovewow");
+        change.innerHTML = helpChar;
+        setTimeout(function(){
+            document.getElementById("inputt").focus();
+            change.classList.remove('textmovewow');
+        }, timevalue*1000)
+
     }
     else if (input.value.toLowerCase() === "Chambon2022") {
+        clear();
         window.open("Chambon2022", '_blank');
     }
     else if (input.value.toLowerCase() === "amor <3") {
+        clear();
         window.open("mcknz", '_blank');
     }
     else if (input.value.toLowerCase() === "secret") {
+        clear();
         window.open(rickroll, '_blank');
     }
     else if (input.value.toLowerCase() === "instagram") {
+        clear();
+        instachar();
+        document.activeElement.blur();
+        change.classList.add("textmovewow");
         change.innerHTML = "Opening Instagram...";
+        setTimeout(function(){
+            window.open(instagram, '_blank');
+            document.getElementById("inputt").focus();
+            change.classList.remove('textmovewow');
+        }, timevalue*1000);
+        
+    }
+    else if (input.value.toLowerCase() === "github") {
+        clear();
+        gitchar();
+        document.activeElement.blur();
+        change.classList.add("textmovewow");
+        change.innerHTML = "Opening Github..."
+        setTimeout(function() {
+            window.open(github, '_blank')
+            document.getElementById("inputt").focus();
+            change.classList.remove("textmovewow");
+        }, timevalue*1000);
     }
     else if (input.value.toLowerCase() === "links") {
-        change.innerHTML = "instagram";
+        clear();
+        instchar2();
+        document.activeElement.blur();
+        insta.classList.add("textmovewow");
+        insta.innerHTML = "instagram";
+        setTimeout(function() {
+            insta.classList.remove("textmovewow");
+        }, timevalue*1000)
+        
+        setTimeout(function() {
+            gittychar2();
+            gitty.classList.add('textmovewow');
+            gitty.innerHTML = "github";
+        }, timevalue*1001)
+
+        setTimeout(function() {
+            gitty.classList.remove('textmovewow');
+            document.getElementById("inputt").focus();
+        }, timevalue*2000)
+        
     }
     else if (input.value.toLowerCase() === "clear") {
-        change.innerHTML = "";
+        clear();
     }
+
     else {
-        console.log("nada")
+        clear();
+        change.innerHTML = helpChar;
     }
     
     
 }
 function after() {
     input.value = "";
+}
+function clear() {
+    change.innerHTML = "";
+    insta.innerHTML = "";
+    gitty.innerHTML = "";
 }
